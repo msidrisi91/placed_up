587c188cc56869a679ff8b70b69eafdd5b7c5c10
msidrisi91 <mohdshaheem91@gmail.com>
2022-04-09 13:30:55 +0530

    session_eleven

---
+++ session_eleven/videochat/public/script.js
@@ -0,0 +1,30 @@
+const socket = io("/");
+
+const videoGrid = document.getElementById("video-grid");
+
+const myPeer = new Peer(undefined, {
+  host: "/",
+  port: "3001",
+});
+
+const peers = {};
+
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+
+    myPeer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+  });
